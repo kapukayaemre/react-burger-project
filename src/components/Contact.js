5ba1903844ff315137c07a5f7affe01af3919fce
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-03-10 22:55:07 +0300

    Contact Page intagrated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,39 @@
 import React from "react";
+import BannerImage from "../assets/banner.png";
+import "../styles/Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle İletişime Geçin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lütfen Adınızı Soyadınızı Giriniz..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lütfen Emailinizi Giriniz..."
+          />
+          <label>Mesajınız</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Lütfen Mesajınızı Giriniz..."
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
