fed0f261a8718d8547b22fdfc9205a4d35ea85d3
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-03-10 22:44:10 +0300

    About page integrated

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,27 @@
 import React from "react";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../styles/About.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkımızda</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque magnam
+          totam fugiat, minima ipsam obcaecati non quam ut beatae? Natus odio
+          cupiditate saepe accusamus, asperiores dolores illum, velit voluptatem
+          beatae suscipit sapiente, facilis molestias aliquid fugiat vitae
+          repudiandae inventore. Laboriosam facere officiis perferendis tenetur
+          animi quis nisi consectetur, totam magnam.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
